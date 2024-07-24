4cefd703263eebef5eb876d36b0c03ea30eb67bc
User <serfintw@yadnex.ru>
2024-07-24 16:19:40 +0300

    update search text boxes

--- tools/remove_text_tool.py
+++ tools/remove_text_tool.py
@@ -1,5 +1,5 @@
 import os
-from typing import List
+from typing import List, Tuple
 
 import cv2
 import numpy as np
@@ -14,12 +14,15 @@ from motleycrew.tools import MotleyTool
 from clear_image.text_deleter import TextDeleter
 from clear_image.inpainter import DalleInpainter, Inpainter
 from clear_image.text_detector import KerasOcrTextDetector, TextBox
+from utils import get_points_density
 
 
 class TextRemover:
 
-    def __init__(self,):
+    def __init__(self, num_text_areas: int = 5, point_threshold: float = 0.1):
         self.api_key = os.environ.get("OPENAI_API_KEY")
+        self.num_text_areas = num_text_areas
+        self.point_threshold = point_threshold
 
     def remove_text(self, image_path: str) -> str:
         image_path = image_path.strip()
@@ -27,7 +30,7 @@ class TextRemover:
             raise FileNotFoundError(image_path)
 
         file_name, ext = os.path.splitext(image_path)
-        output_image_path = "{}_remove_text{}".format(file_name,ext)
+        output_image_path = "{}_remove_text{}".format(file_name, ext)
         text_detector = KerasOcrTextDetector()
         inpainter = DalleInpainter(self.api_key)
         text_deleter = TextDeleter(text_detector, inpainter)
@@ -35,10 +38,16 @@ class TextRemover:
 
         ret_rows = ["Clear image path: {}".format(ret_image_path)]
 
+        boxes = []
         if text_deleter.text_bboxes:
+            boxes = self.find_text_boxes(image_path, inpainter, text_deleter.text_bboxes)
+        else:
+            text_area = self.find_discharged_area(image_path)
+            boxes = [text_area]
+
+        if boxes:
             ret_rows.append("Text boxes:")
             text_box_template = "    text box: x: {}, y: {}, width: {}, height: {}"
-            boxes = self.find_text_boxes(image_path, inpainter, text_deleter.text_bboxes)
             for box in boxes:
                 ret_rows.append(text_box_template.format(*box))
         else:
@@ -46,7 +55,9 @@ class TextRemover:
 
         return "\n".join(ret_rows)
 
-    def find_text_boxes(self, image_path: str, inpainter: Inpainter, text_boxes: List[TextBox] ):
+    def find_text_boxes(
+        self, image_path: str, inpainter: Inpainter, text_boxes: List[TextBox]
+    ) -> List[Tuple[int, int, int, int]]:
         img = cv2.imread(image_path)
         h, w = img.shape[:2]
 
@@ -63,17 +74,30 @@ class TextRemover:
         for label in range(1, num_labels):  # Start from 1 to skip the background
             x, y, w, h = cv2.boundingRect((labels_im == label).astype(np.uint8))
             boxes.append((x, y, w, h))
-            img_mask = cv2.rectangle(img_mask, (x, y), (x+w, y+h), (255, 0, 0), 3)
+            img_mask = cv2.rectangle(img_mask, (x, y), (x + w, y + h), (255, 0, 0), 3)
 
         boxes.sort(key=lambda x: x[2] * x[3], reverse=True)
         return boxes
 
+    def find_discharged_area(self, image_path: str) -> Tuple[int, int, int, int]:
+        img = cv2.imread(image_path)
+        h, w = img.shape[:2]
+
+        areas_data = get_points_density(img, self.num_text_areas, self.point_threshold)
+        for d in areas_data:
+            print(d)
+        y, y_max = areas_data[1][0]
+        x = int(w * 0.05)
+        x_max = w - x
+        return x, y, x_max - x, y_max - y
+
 
 class RemoveTextTool(MotleyTool):
 
-    def __init__(self,):
-        """Tool for removing text from image
-        """
+    def __init__(
+        self,
+    ):
+        """Tool for removing text from image"""
         remover = TextRemover()
         langchain_tool = create_render_tool(remover)
         super().__init__(langchain_tool)
