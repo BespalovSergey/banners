e2114fcdb56f5e317caf72624513bc04d6ce037b
User <serfintw@yadnex.ru>
2024-08-26 18:14:32 +0300

    add UiBannerGeneratorWithText class

---
+++ ui/generator_with_ui.py
@@ -0,0 +1,33 @@
+from typing import List, Tuple
+
+
+from motleycrew.tools import MotleyTool
+from generator import BannerGeneratorWithText
+from checkers import BaseChecker
+
+
+class UiBannerGeneratorWithText(BannerGeneratorWithText):
+
+    ui_state_name = "ui_banner_generator_with_text"
+
+    def __init__(
+        self,
+        image_description: str,
+        text_description: str,
+        images_dir: str,
+        slogan: str,
+        html_render_checkers: List[BaseChecker] = None,
+        image_size: Tuple[int, int] = (1024, 1024),
+        max_review_iterations: int = 5,
+        image_generate_tool: MotleyTool = None
+    ):
+        super().__init__(
+            image_description=image_description,
+            text_description=text_description,
+            images_dir=images_dir,
+            slogan=slogan,
+            html_render_checkers=html_render_checkers,
+            image_size=image_size,
+            max_review_iterations=max_review_iterations,
+            image_generate_tool=image_generate_tool
+        )
